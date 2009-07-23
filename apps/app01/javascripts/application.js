212500712d26acc7d97a858a82582e242da9313c
unknown <guptabh@.merck.com>
2009-07-23 03:42:57 -0400

    COMMIT5

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -71,7 +71,9 @@ var copyPreviousObjectiveHandler = function() {
 
 var augmentCallDetailsEntry = function() {
 	var row =  "<tr width='100%'>";
-	row += "<td>Product: <select><option></option><option>Singulair</option><option>Hyzaar</option></td>";
+	row += "<td><span class="requiredText">Product*</span><input type='text' size='7'></input>
+<img class="popup" onclick="openAssocPopup('AssocProductPopup?mapBC=Pharma+Call+Products+Detailed&#38;OACTRL=Name&#38;ophi=CallProdDetailNew.Product+Id&#38;pfid=CallProdDetailNew&#38;OMTHD=AssocPopup&#38;OMTGT=PopupSearchList&#38;assocInit=Y&#38;opht=4&#38;OAOBJ=Call+ProdDetail&#38;mapField=Name&#38;ophd=CallProdDetailNew.Name&#38;ophpd=1&#38;disableclear=Y&#38;ophr=AssocProductPopup',350,500,'OccamPopup1','CallProdDetailNew.Product Id',true,false);" src="../1.10.0.1079.0.03/base/themes/oracle/images/iconSearch.gif" />
+</td>";
 	row += "<td>Priority: <input type='text' size='1'></input></td>";
 	row += "<td>Indication: <select><option></option><option>Allergy</option><option>Asthma</option></td>";
 	row += "<td>Issues: <select><option></option><option>Side effects</option><option>Efficacy</option></td>";
