0cad5c9d7057e360c3127f8b3686f962ff2aee0c
unknown <guptabh@.merck.com>
2009-07-24 00:08:51 -0400

    COMMIT19

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -71,7 +71,7 @@ var copyPreviousObjectiveHandler = function() {
 
 var augmentCallDetailsEntry = function() {
 	var row =  "<tr width='100%'>";
-	row += "<td>Product: <p><a href="https://secure-ausomxapa.crmondemand.com/OnDemand/user/CallProdDetailNew?OMRET1=ContactCallDetail%3fOMTGT%3dContactCallForm%26OMTHD%3dActivityDetailNav%26ContactCallForm.Id%3dAAPA-2K60K2&OMCR0=AAPA-2K60K2&OMTGT=CallProdDetailNew&OMTHD=CallProdDetailNewNav&OMCBO=Action&OMRET0=ContactDetail%3focTitle%3dJack%2bHandey%26OMTGT%3dContactDetailForm%26OMTHD%3dContactDetailNav%26ocEdit%3dY%26OCTYPE%3dCustomer%26ocTitleField%3dFull%2bName%26ContactDetailForm.Id%3dAAPA-2GEFTS&OCNOEDITTYPE=Y">
+	row += "<td>Product:<p><a href="https://secure-ausomxapa.crmondemand.com/OnDemand/user/CallProdDetailNew?OMRET1=ContactCallDetail%3fOMTGT%3dContactCallForm%26OMTHD%3dActivityDetailNav%26ContactCallForm.Id%3dAAPA-2K60K2&OMCR0=AAPA-2K60K2&OMTGT=CallProdDetailNew&OMTHD=CallProdDetailNewNav&OMCBO=Action&OMRET0=ContactDetail%3focTitle%3dJack%2bHandey%26OMTGT%3dContactDetailForm%26OMTHD%3dContactDetailNav%26ocEdit%3dY%26OCTYPE%3dCustomer%26ocTitleField%3dFull%2bName%26ContactDetailForm.Id%3dAAPA-2GEFTS&OCNOEDITTYPE=Y">
                  <img border="0" src="../1.10.0.1079.0.03/base/themes/oracle/images/iconSearch.gif" width="65" height="38"></a></p></td>";
 	row += "<td>Priority: <input type='text' size='1'></input></td>";
 	row += "<td>Indication: <select><option></option><option>Allergy</option><option>Asthma</option></td>";
